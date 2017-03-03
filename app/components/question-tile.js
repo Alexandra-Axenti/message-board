e922d4ba629391d1c2194334244022018c0c5859
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-03 17:36:17 +0800

    last mandatory update

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // isNotesShowing: false,
-  //
-  // actions: {
-  //   notesShow: function() {
-  //     this.set('isNotesShowing', true);
-  //   },
-  //   notesHide: function() {
-  //     this.set('isNotesShowing', false);
-  //   }
-  // }
 });
